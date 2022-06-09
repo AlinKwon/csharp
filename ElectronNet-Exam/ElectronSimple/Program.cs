a7aa02f4b7e5dfc98e307e1b6eed8bddfb1a0b6e
alinkwon <alin.kwon@kakao.com>
2022-06-09 11:40:27 +0900

    nlog

--- ElectronNet-Exam/ElectronSimple/Program.cs
+++ ElectronNet-Exam/ElectronSimple/Program.cs
@@ -1,5 +1,11 @@
 using ElectronNET.API;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using NLog;
+using NLog.Web;
 
 namespace ElectronSimple
 {
@@ -7,13 +13,35 @@ namespace ElectronSimple
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("init main");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                //NLog: catch setup errors
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
-                .ConfigureLogging((hostingContext, logging) => { logging.AddConsole(); })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog()
                 .UseElectron(args)
                 .UseStartup<Startup>();
         }
