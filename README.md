1b462d3bffda8ade2a8499696d5b04ea1ac76855
alinkwon <alin.kwon@kakao.com>
2022-06-21 15:23:01 +0900

    validation example

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ csharp example
 1. ElectronNet-Exam : Electron.net sample
 2. StreamJsonRpcSample : json rpc .net sample
 
+(read env)[https://docs.microsoft.com/ko-kr/dotnet/api/system.environment.setenvironmentvariable?view=net-6.0]
---
+++ ValidationSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.AccessControl;
+
+public class Target
+{
+    [Required(AllowEmptyStrings =true, ErrorMessage = "In..")]
+    //[Range(4,10)]
+    //[StringLength(4)]
+    [MaxLength(10,ErrorMessage="> big")]
+    [MinLength(4,ErrorMessage="< small")]
+    public string? Code { get; set; }
+} ;
+
+
+//Validator.TryValidateValue(a);
+
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var target = new Target
+            {
+                Code = " " //null""// "123"
+            };
+
+            var context = new ValidationContext(target, serviceProvider: null, items: null);
+            var validationResults = new List<ValidationResult>();
+            Console.WriteLine(Validator.TryValidateObject(target, context, validationResults, true)) ;
+
+            foreach (var result in validationResults){
+                Console.WriteLine(result.ErrorMessage) ;
+            }
+            
+        }
+    }
+}
