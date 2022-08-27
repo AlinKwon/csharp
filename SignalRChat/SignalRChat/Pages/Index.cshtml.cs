043c6f6e65d4f591f851fb5f9bd647fdd1dc8ff5
alinkwon <alin.kwon@kakao.com>
2022-08-27 22:10:10 +0900

    signalR1

--- SignalRChat/SignalRChat/Pages/Index.cshtml.cs
+++ SignalRChat/SignalRChat/Pages/Index.cshtml.cs
@@ -1,20 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.SignalR;
+using SignalRChat.Hubs;
 
 namespace SignalRChat.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IHubContext<ChatHub> chatHub;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger
+            ,IHubContext<ChatHub> chatHub)
         {
             _logger = logger;
+            this.chatHub = chatHub;
         }
 
         public void OnGet()
         {
 
+        }
+
+        public async void OnGetSend()
+        {
+            await chatHub.Clients.All.SendAsync("ReceiveMessage", "server", "hellow, clients!!");
         }
     }
 }
