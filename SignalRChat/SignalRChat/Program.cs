f7d8f73abdcd23ffbc17f17dd650fd496888f356
alinkwon <alin.kwon@kakao.com>
2022-08-27 21:54:40 +0900

    signalR

---
+++ SignalRChat/SignalRChat/Program.cs
@@ -0,0 +1,28 @@
+using SignalRChat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//app.UseAuthorization();
+
+
+//app.UseCors();
+app.MapRazorPages();
+
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
--- StreamJsonRpcSample/SJRClient/Program.cs
+++ StreamJsonRpcSample/SJRClient/Program.cs
@@ -6,12 +6,13 @@ using System.IO;
 using System.IO.Pipes;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
 
 namespace SJRClient
 {
     internal class Program
     {
+#if true
         static async Task Main(string[] args)
         {
             using (var stream = new NamedPipeClientStream(".", "StreamJsonRpcSamplePipe", PipeDirection.InOut, PipeOptions.Asynchronous))
@@ -57,10 +58,70 @@ namespace SJRClient
             Console.WriteLine($"Param = {sum.Name}");
         }
 
-        
+
         internal class Param
         {
             public string Name { get; set; }
+        } 
+#endif
+#if false
+
+        static async Task Main(string[] args)
+        {
+            using (var stream = new NamedPipeClientStream(".", "vuruDongleServicePipe", PipeDirection.InOut, PipeOptions.Asynchronous))
+            {
+                await stream.ConnectAsync();
+                using (var jsonRpc = JsonRpc.Attach(stream))
+                {
+                    while (true)
+                    {
+                        var cmd = Console.ReadLine();
+                        switch (cmd)
+                        {
+                            case "1":
+                                await ActAsRpcClientAsync(jsonRpc);
+                                break;
+                            case "2":
+                                await ActAsRpcClientAsync2(jsonRpc);
+                                break;
+                            case "0":
+                                goto outerLoop;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                outerLoop:
+                    Console.WriteLine("Terminating stream...");
+                }
+            }
         }
+
+        private static async Task ActAsRpcClientAsync(JsonRpc jsonRpc)
+        {
+            Console.WriteLine("Connected. Sending request...");
+            int sum = await jsonRpc.InvokeAsync<int>("Add", 3, 5);
+            Console.WriteLine($"3 + 5 = {sum}");
+        }
+
+        public class Validation
+        {
+            public string PCK { get; set; }
+            public string ACK { get; set; }
+        }
+
+        private static async Task ActAsRpcClientAsync2(JsonRpc jsonRpc)
+        {
+            Console.WriteLine("Connected. Sending request...");
+            Validation sum = await jsonRpc.InvokeAsync<Validation>("Valid", new Validation { PCK = "안녕" });
+            Console.WriteLine($"Param = {sum.PCK}");
+        }
+
+
+        internal class Param
+        {
+            public string Name { get; set; }
+        }  
+#endif
     }
 }
